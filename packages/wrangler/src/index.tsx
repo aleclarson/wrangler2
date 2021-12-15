dff904d3aa061772f5803b14aa739fce95f7e5e9
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-12-15 17:25:45 -0500

    chore: import React as namespace
    otherwise Rollup complains

--- packages/wrangler/src/index.tsx
+++ packages/wrangler/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { render } from "ink";
 import { Dev } from "./dev";
 import { readFile } from "fs/promises";
--- packages/wrangler/src/scopes.tsx
+++ packages/wrangler/src/scopes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { render } from "ink";
 import Table from "ink-table";
 import { throwIfNotInitialised } from "./user";
